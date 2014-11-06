521e3147083b27db79a984e70ed85dccc5c9fd55
hAMO <hamo@hamo-UX32VD.(none)>
2014-11-06 02:53:15 -0800

    More work on the chess problem.

--- rope-intranet/src/main/java/com/codejam/ChessBoards.groovy
+++ rope-intranet/src/main/java/com/codejam/ChessBoards.groovy
@@ -9,12 +9,90 @@ package com.codejam
  */
 class ChessBoards
 {
-    def cutChessBoards(int[][] inputMatrix) {
-        
+    static def cutChessBoards(char [][] inputMatrix) {
+        def chessBoardSizes = [:]
+        chessBoardSizes.put(1, new LinkedHashSet())
+
+        //add all coordinates as candidates of size 1 chessboards.
+        for (int i = 0; i < inputMatrix.size(); i++) {
+            for (int j = 0; j < inputMatrix[i].size(); j++) {
+                chessBoardSizes.get(1).add([i, j])
+            }
+
+        }
+
+        //start with size 2 and move up until no larger boards can be built
+        for (int size = 2; size < inputMatrix[0].size(); size++) {
+            def previousSizeCoordinates = chessBoardSizes.get(size-1)
+
+            if (previousSizeCoordinates.isEmpty()) { break }
+
+            chessBoardSizes.put(size, new LinkedHashSet())
+
+            def currentSizeSet = chessBoardSizes.get(size)
+
+            previousSizeCoordinates.removeAll { pair ->
+                if (isSquareOfSize(size, pair, inputMatrix)) {
+                    currentSizeSet.add(pair)
+                    true
+                } else {
+                    false
+                }
+            }
+
+            previousSizeCoordinates.removeAll { pair -> hasOverLap(size, pair, currentSizeSet) }
+        }
 
 
-
+        return chessBoardSizes;
 	}
+
+    static boolean hasOverLap(int size, def previousSizePair, def currentSizeSet) {
+        currentSizeSet.any { currentSizePair -> rectOverlap(size, previousSizePair, currentSizePair) }
+
+    }
+
+    static boolean valueInRange(int value, int min, int max){ return (value >= min) && (value <= max); }
+
+    static boolean rectOverlap(int size, def previousSizePair, def currentSizePair) {
+        boolean xOverlap = valueInRange(previousSizePair[0], currentSizePair[0], currentSizePair[0] + size) ||
+                valueInRange(currentSizePair[0], previousSizePair[0], previousSizePair[0]+ size - 1);
+
+        boolean yOverlap = valueInRange(previousSizePair[1], currentSizePair[1], currentSizePair[1] + size) ||
+                valueInRange(currentSizePair[1], previousSizePair[1], previousSizePair[1]+ size - 1);
+
+        return xOverlap && yOverlap;
+    }
+
+    static boolean isSquareOfSize(int size, def coordinatePair, char[][] inputMatrix) {
+        if (coordinatePair[0] + size > inputMatrix.size() || coordinatePair[1] + size > inputMatrix[0].size()) {
+            return false
+        }
+
+        int rightIndex = coordinatePair[1] + size - 1
+        if (inputMatrix[coordinatePair[0]][rightIndex-1] as int ^ inputMatrix[coordinatePair[0]][rightIndex] as int) {
+            for (int i = coordinatePair[0]; i < coordinatePair[0] + size -1; i++) {
+                if (!xor(inputMatrix[i][coordinatePair[1]..rightIndex], inputMatrix[i+1][coordinatePair[1]..rightIndex])) {
+                    return false
+                }
+            }
+
+            return true
+        } else {
+            return false
+        }
+    }
+
+    static boolean xor (def a, def b )
+    {
+        for ( int i=0; i<a.size() ; i++ )
+        {
+            if (!(a[i] as int ^ b[i] as int)) {
+                return false
+            }
+        }
+        return true
+    }
 
 	static void main(String[] args) {
 		def testInput = new File(args[0]).newReader()
