e428086ae75609a724a31997209c1bdb02967613
hAMO <hamo@hamo-UX32VD.(none)>
2014-11-14 06:26:11 -0800

    Fixed prisoner solution to run 20 times faster.

--- code-jam-challenges/src/main/java/com/codejam/Prisoners.groovy
+++ code-jam-challenges/src/main/java/com/codejam/Prisoners.groovy
@@ -31,37 +31,29 @@ class Prisoners {
         // We can memoize ranges and optimal bribe values when found.
         def cache = [:]
 
-        def minBribes = calcBribes(1, numPrisonCells, prisonersToRelease, cache)
+        def minBribes = calcBribes(1, numPrisonCells, new HashSet(prisonersToRelease.collect { Integer.valueOf(it) }), cache)
 
         return minBribes
 
     }
 
     static def calcBribes(def startingIndex, def endIndex, def prisonersToRelease, def cache) {
-        def prisonersToConsider = []
-
-        for (int i = startingIndex; i <= endIndex; i++) {
-            if (prisonersToRelease.contains(String.valueOf(i))) {
-                prisonersToConsider.add(i)
-            }
-        }
-
-        if (prisonersToConsider.isEmpty()) {
+        if (prisonersToRelease.isEmpty()) {
             cache.put([startingIndex, endIndex], 0)
             return 0
         }
 
-        def minBribeValue = prisonersToConsider.collect { prisonerIndex ->
+        def minBribeValue = prisonersToRelease.collect { prisonerIndex ->
             def (optimalRightBribes, optimalLeftBribes) = [0, 0]
 
             if (prisonerIndex + 1 < endIndex) {
                 optimalRightBribes = cache.containsKey([prisonerIndex + 1, endIndex]) ? cache.get([prisonerIndex + 1, endIndex]) :
-                        calcBribes(prisonerIndex +1, endIndex, prisonersToRelease, cache)
+                        calcBribes(prisonerIndex +1, endIndex, prisonersToRelease.findAll{ it > prisonerIndex}, cache)
             }
 
             if (prisonerIndex - 1 > startingIndex) {
                 optimalLeftBribes = cache.containsKey([startingIndex, prisonerIndex - 1]) ? cache.get([startingIndex, prisonerIndex - 1]) :
-                        calcBribes(startingIndex, prisonerIndex - 1, prisonersToRelease, cache)
+                        calcBribes(startingIndex, prisonerIndex - 1, prisonersToRelease.findAll{ it < prisonerIndex }, cache)
             }
 
 
